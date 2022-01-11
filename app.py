f0422db08be07dd1248406ae82949f9ef8b4dc6f
thanapoom-nisamaneewong <74890654+thanapoom-nisamaneewong@users.noreply.github.com>
2022-01-11 14:20:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def app():
     class_btn = st.button("Extract")
 
     if file_uploaded is not None:
-        with open(os.path.join("/Users/micky/ever/form/form_test",file_uploaded.name),"wb") as f:
+        with open(os.path.join("form_test",file_uploaded.name),"wb") as f:
             f.write((file_uploaded).getbuffer())
 
         file_path=f'/Users/micky/ever/form/form_test/{file_uploaded.name}'
